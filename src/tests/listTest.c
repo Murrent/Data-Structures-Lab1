ddb2733f9290cf3048b4d9e085278c677978060e
Tempron <wickgrenm@gmail.com>
2021-02-19 12:04:53 +0100

    ex added

--- src/tests/listTest.c
+++ src/tests/listTest.c
@@ -7,75 +7,75 @@
 // MAXIMUM TESTS
 void maximumTest1(List* list) {
     if (list == NULL || maximum(list) == NULL || maximum(list)->key != 10)
-        printf("maximum test failed\n");
+        printf("maximum test 1 failed\n");
     else
-        printf("maximum test succeeded\n");
+        printf("maximum test 1 succeeded\n");
 }
 
 void maximumTest2(List* list) {
     if (list == NULL || maximum(list) == NULL || maximum(list)->key != 9)
-        printf("maximum test failed\n");
+        printf("maximum test 2 failed\n");
     else
-        printf("maximum test succeeded\n");
+        printf("maximum test 2 succeeded\n");
 }
 
 // MINIMUM TESTS
 void minimumTest1(List* list) {
     if (list == NULL || minimum(list) == NULL || minimum(list)->key != 1)
-        printf("minimum test failed\n");
+        printf("minimum test 1 failed\n");
     else
-        printf("minimum test succeeded\n");
+        printf("minimum test 1 succeeded\n");
 }
 
 void minimumTest2(List* list) {
     if (list == NULL || minimum(list) == NULL || minimum(list)->key != 1)
-        printf("minimum test failed\n");
+        printf("minimum test 2 failed\n");
     else
-        printf("minimum test succeeded\n");
+        printf("minimum test 2 succeeded\n");
 }
 
 // SUCCESSOR TESTS
 void successorTest1(List* list) {
-    if (list == NULL || successor(list, search(list, 5)) == NULL)
-        printf("successor test failed\n");
-    else if (successor(list, search(list, 5))->key == 7)
-        printf("successor test succeeded\n");
+    if (list == NULL || successor(list, search(list, 5)) == NULL || (successor(list, search(list, 5))->key != 7))
+        printf("successor test 1 failed\n");
+    else
+        printf("successor test 1 succeeded\n");
 }
 
 void successorTest2(List* list) {
     if (list == NULL || successor(list, search(list, 9)) != NULL)
-        printf("successor test failed\n");
+        printf("successor test 2 failed\n");
     else
-        printf("successor test succeeded\n");
+        printf("successor test 2 succeeded\n");
 }
 
 // PREDECESSOR TESTS
 void predecessorTest1(List* list) {
-    if (list == NULL || predecessor(list, search(list, 5)) == NULL)
-        printf("predecessor test failed\n");
-    else if (predecessor(list, search(list, 5))->key == 3)
-        printf("predecessor test succeeded\n");
+    if (list == NULL || predecessor(list, search(list, 5)) == NULL || (predecessor(list, search(list, 5))->key != 3))
+        printf("predecessor test 1 failed\n");
+    else
+        printf("predecessor test 1 succeeded\n");
 }
 
 void predecessorTest2(List* list) {
-    if (list == NULL || predecessor(list, search(list, 9)) == NULL)
-        printf("predecessor test failed\n");
-    else if (predecessor(list, search(list, 9))->key == 6)
-        printf("predecessor test succeeded\n");
+    if (list == NULL || predecessor(list, search(list, 9)) == NULL || (predecessor(list, search(list, 9))->key != 6))
+        printf("predecessor test 2 failed\n");
+    else
+        printf("predecessor test 2 succeeded\n");
 }
 
-void predecessorTest3(List* list) {
-    if (list == NULL || predecessor(list, maximum(list)) == NULL)
-        printf("predecessor test failed\n");
-    else if (predecessor(list, maximum(list))->key == 8)
-        printf("predecessor test succeeded\n");
+void predecessorTest3(List* list1, List* list2) {
+    if (list2 == NULL || predecessor(list2, maximum(list1)) == NULL || (predecessor(list2, maximum(list1))->key != 9))
+        printf("predecessor test 3 failed\n");
+    else
+        printf("predecessor test 3 succeeded\n");
 }
 
-void predecessorTest4(List* list) {
-    if (list == NULL || predecessor(list, maximum(list)) == NULL)
-        printf("predecessor test failed\n");
-    else if (predecessor(list, maximum(list))->key == 6)
-        printf("predecessor test succeeded\n");
+void predecessorTest4(List* list1, List* list2) {
+    if (list1 == NULL || predecessor(list1, maximum(list2)) == NULL || (predecessor(list1, maximum(list2))->key != 8))
+        printf("predecessor test 4 failed\n");
+    else
+        printf("predecessor test 4 succeeded\n");
 }
 
 void testList() {
@@ -111,8 +111,8 @@ void testList() {
 
     predecessorTest1(list1);
     predecessorTest2(list2);
-    predecessorTest3(list1);
-    predecessorTest4(list2);
+    predecessorTest3(list1, list2);
+    predecessorTest4(list1, list2);
 
     freeList(list1);
     freeList(list2);
